773faebed64c1b6ab5bb7b7d300dea1341aad69b
Jason Parham <jason.parham@kitware.com>
2026-02-02 22:39:13 -0500

    Explicit Numpy 2.0 ABI

--- src/cpp/astar.cpp
+++ src/cpp/astar.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <Python.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#define NPY_TARGET_VERSION NPY_2_0_API_VERSION
 #include <numpy/arrayobject.h>
 #include <iostream>
 #include <experimental_heuristics.h>
